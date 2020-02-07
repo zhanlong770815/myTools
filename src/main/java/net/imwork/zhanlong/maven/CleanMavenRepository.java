fa5fa5820172a69c93d43c978279ca06e31eb0cc
昝海龙-家 <120769563@qq.com>
2020-02-07 15:19:32 +0800

    增加清除maven仓库的工具

---
+++ src/main/java/net/imwork/zhanlong/maven/CleanMavenRepository.java
@@ -0,0 +1,71 @@
+package net.imwork.zhanlong.maven;
+
+import java.io.File;
+
+/**
+ * @author 昝海龙
+ * 清除maven仓库中错误文件或空文件夹
+ *
+ */
+public class CleanMavenRepository
+{
+    /**
+     * root: 仓库根目录,此处为本地maven仓库的绝对路径
+     */
+    static String root = "D:\\java_studying\\repository";
+
+    public static void main(String[] args) {
+        File file = new File(root);
+        File[] files = file.listFiles();
+        if (files != null && files.length > 0) {
+            for (File f : files) {
+                clearRepository(f);
+            }
+        }
+    }
+
+    public static boolean clearRepository(File file) {
+        boolean isHaveJar = false;
+        File[] files = file.listFiles();
+        if (files != null && files.length > 0) {
+            for (File aFile : files) {
+                // 判断是否为jar文件
+                if (aFile.getName().endsWith(".jar")) {
+                    isHaveJar = true;
+                }
+                // 判断是否为文件夹
+                if (aFile.isDirectory()) {
+                    // 是文件夹则判断其内部是否包含jar文件
+                    boolean isNextHaveJar = clearRepository(aFile);
+                    if (isNextHaveJar) {
+                        isHaveJar = true;
+                    }
+                }
+            }
+        }
+        if (!isHaveJar) {
+            System.out.println("Delete The File: " + file.getName());
+            delete(file);
+        }
+        return isHaveJar;
+    }
+
+    /**
+     * 删除文件或文件夹
+     * @param file 要删除的文件或文件夹
+     */
+    public static void delete(File file) {
+        File[] files = file.listFiles();
+        if (files != null && files.length > 0) {
+            for (File aFile : files) {
+                if (aFile.isDirectory()) {
+                    delete(aFile);
+                }
+                aFile.delete();
+            }
+        } else {
+            file.delete();
+        }
+    }
+}
+
