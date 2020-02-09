dcf9614889bb58b5b22df0735525ff412bbf15cf
昝海龙-家 <120769563@qq.com>
2020-02-09 09:31:47 +0800

    edit

--- src/main/java/net/imwork/zhanlong/maven/CleanMavenRepository.java
+++ src/main/java/net/imwork/zhanlong/maven/CleanMavenRepository.java
@@ -5,7 +5,6 @@ import java.io.File;
 /**
  * @author 昝海龙
  * 清除maven仓库中错误文件或空文件夹
- *
  */
 public class CleanMavenRepository
 {
@@ -26,59 +25,65 @@ public class CleanMavenRepository
     public static void clean()
     {
         String rootDirectory = "D:\\java_studying\\repository";
-        System.out.println("清除目录:" + rootDirectory);
+        System.out.print("清除目录:" + rootDirectory);
         File file = new File(rootDirectory);
         File[] files = file.listFiles();
-        if (files != null && files.length > 0) {
-            for (File f : files) {
+        if (files != null && files.length > 0)
+        {
+            for (File f : files)
+            {
                 clearRepository(f);
             }
         }
     }
+
     /**
-     *
      * @param rootDirectory
      */
-    public static void clean(String rootDirectory ) {
-
-        if (rootDirectory == null || rootDirectory.isEmpty())
-        {
-            return;
-        }
-        System.out.println("清除目录:" + rootDirectory);
+    public static void clean(String rootDirectory)
+    {
         rootDirectory = "d:\\repository\\" + rootDirectory;
+        System.out.println("清除目录:" + rootDirectory);
 
         File file = new File(rootDirectory);
         File[] files = file.listFiles();
-        if (files != null && files.length > 0) {
-            for (File f : files) {
+        if (files != null && files.length > 0)
+        {
+            for (File f : files)
+            {
                 clearRepository(f);
             }
         }
     }
 
 
-
-    private static boolean clearRepository(File file) {
+    private static boolean clearRepository(File file)
+    {
         boolean isHaveJar = false;
         File[] files = file.listFiles();
-        if (files != null && files.length > 0) {
-            for (File aFile : files) {
+        if (files != null && files.length > 0)
+        {
+            for (File aFile : files)
+            {
                 // 判断是否为jar文件
-                if (aFile.getName().endsWith(".jar")) {
+                if (aFile.getName().endsWith(".jar"))
+                {
                     isHaveJar = true;
                 }
                 // 判断是否为文件夹
-                if (aFile.isDirectory()) {
+                if (aFile.isDirectory())
+                {
                     // 是文件夹则判断其内部是否包含jar文件
                     boolean isNextHaveJar = clearRepository(aFile);
-                    if (isNextHaveJar) {
+                    if (isNextHaveJar)
+                    {
                         isHaveJar = true;
                     }
                 }
             }
         }
-        if (!isHaveJar) {
+        if (!isHaveJar)
+        {
             System.out.println("Delete The File: " + file.getName());
             delete(file);
         }
@@ -87,18 +92,24 @@ public class CleanMavenRepository
 
     /**
      * 删除文件或文件夹
+     *
      * @param file 要删除的文件或文件夹
      */
-    private static void delete(File file) {
+    private static void delete(File file)
+    {
         File[] files = file.listFiles();
-        if (files != null && files.length > 0) {
-            for (File aFile : files) {
-                if (aFile.isDirectory()) {
+        if (files != null && files.length > 0)
+        {
+            for (File aFile : files)
+            {
+                if (aFile.isDirectory())
+                {
                     delete(aFile);
                 }
                 aFile.delete();
             }
-        } else {
+        } else
+        {
             file.delete();
         }
     }
